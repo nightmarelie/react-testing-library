bedf54aa1a013c1d8158ef3e566f8fb479d1b8c1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-16 20:05:04 -0600

    test: cover jest dom assertions

--- src/__tests__/jest-dom.js
+++ src/__tests__/jest-dom.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import '@testing-library/jest-dom/extend-expect';
 import { FavoriteNumber } from '../favorite-number';
 
 test('renders a number input with a label "Favorite Number"', () => {
   const div = document.createElement('div');
   ReactDom.render(<FavoriteNumber />, div);
-  expect(div.querySelector('input').type).toBe('number');
+  expect(div.querySelector('input')).toHaveAttribute('type', 'number');
+  expect(div.querySelector('label')).toHaveTextContent('Favorite Number');
 });
