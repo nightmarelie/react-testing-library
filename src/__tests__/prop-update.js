f43f9c202421deec6ce609c5e921aa0af1ca6b15
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-17 17:05:30 -0600

    test: rerender component

---
+++ src/__tests__/prop-update.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import user from '@testing-library/user-event';
+import { render } from '@testing-library/react';
+import { FavoriteNumber } from '../favorite-number';
+
+test('entering an invalid value shows an error message', () => {
+  const { getByLabelText, getByRole, queryByRole, rerender } = render(
+    <FavoriteNumber min={2} max={3} />,
+  );
+
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const input = getByLabelText(/favorite number/i);
+
+  user.type(input, '10');
+
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
+
+  rerender(<FavoriteNumber max={12} />);
+
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  expect(queryByRole('alert')).not.toBeInTheDocument();
+});
