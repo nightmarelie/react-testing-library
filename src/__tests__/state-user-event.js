5baa09df9cf898d2ce610f993d4982885980a378
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-17 16:59:16 -0600

    test: write resemble user test

--- src/__tests__/state-user-event.js
+++ src/__tests__/state-user-event.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import { FavoriteNumber } from '../favorite-number';
 
 test('entering an invalid value shows an error message', () => {
@@ -9,7 +10,8 @@ test('entering an invalid value shows an error message', () => {
 
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i);
-  fireEvent.change(input, { target: { value: '10' } });
+
+  user.type(input, '10');
 
   // eslint-disable-next-line testing-library/prefer-screen-queries
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
