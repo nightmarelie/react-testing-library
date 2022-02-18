cd108cbf6c687a0b15e680997238108ab8420b9f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-02-18 16:56:08 -0600

    test: refactor react router case

--- src/__tests__/react-router-01-02.js
+++ src/__tests__/react-router-01-02.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { screen, render } from '@testing-library/react';
+import { screen, render as rtlRender } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Main } from '../main';
 
+function render(ui, options) {
+  function Wrapper({ children }) {
+    return <BrowserRouter>{children}</BrowserRouter>;
+  }
+  return rtlRender(ui, {
+    wrapper: Wrapper,
+    ...options,
+  });
+}
+
 test('main renders about and home and I can navigate to those pages', () => {
   window.history.pushState({}, 'Test page', '/');
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  );
+  render(<Main />);
 
   expect(screen.getByRole('heading')).toHaveTextContent(/home/i);
   userEvent.click(screen.getByText(/about/i));
@@ -19,11 +25,7 @@ test('main renders about and home and I can navigate to those pages', () => {
 
 test('landing on a bad page shows no match component', () => {
   window.history.pushState({}, 'Test page', '/something-that-does-not-match');
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  );
+  render(<Main />);
 
   expect(screen.getByRole('heading')).toHaveTextContent(/404/i);
 });
